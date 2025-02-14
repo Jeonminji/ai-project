d084b9e0b764638fdc6f12ee21373916118e68fb
Imji <minjia13@gmail.com>
2025-02-14 10:44:19 +0900

    채팅 controller 추가

--- src/main/java/com/min/aiproject/controller/ai/AiController.java
+++ src/main/java/com/min/aiproject/controller/ai/AiController.java
@@ -1,4 +1,15 @@
 package com.min.aiproject.controller.ai;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Tag(name = "1. AI Chat", description = "채팅")
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
 public class AiController {
 }
