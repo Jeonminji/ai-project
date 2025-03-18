39bedd2b8cc42c56f1ac617139b08a6bca010b7d
Jeonminji <minjia13@gmail.com>
2025-03-18 15:28:19 +0900

    db 연동 완료

--- src/main/java/com/min/aiproject/AiProjectApplication.java
+++ src/main/java/com/min/aiproject/AiProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @Slf4j
+@SpringBootApplication
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AiProjectApplication {
 
